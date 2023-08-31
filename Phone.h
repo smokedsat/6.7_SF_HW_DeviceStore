79fd5f4269ee9dd3c02abd8fe18fb2c28c0e833c
Smokedsat <65419292+smokedsat@users.noreply.github.com>
2023-08-31 11:59:59 +0300

    Update Phone.h

--- Phone.h
+++ Phone.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "Shop.h"
 
-class Phone  : virtual public Device, public MoreChar
+class Phone  : virtual public Device, public MoreCharacteristics
 {
 public:
-	Phone(int count, int price, string IMEI, string company ,int memory, string processor, bool updatable, bool touchPad) :Device(count, price, IMEI, company), MoreChar(memory, processor, updatable), _touchPad(touchPad) {};
+	Phone(int count, int price, std::string & IMEI, std::string & company ,int memory, std::string & processor, bool updatable, bool touchPad) :Device(count, price, IMEI, company), MoreCharacteristics(memory, processor, updatable), _touchPad(touchPad) {};
 	virtual void showCharacteristics() override;
 	virtual void buy(int count) override;
 	virtual ~Phone() {};
 protected:
 	bool _touchPad;
 };
+
+
 
