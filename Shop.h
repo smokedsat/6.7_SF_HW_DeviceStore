b814d4a508caf76c3df9c907849fef056828f5b0
Smokedsat <65419292+smokedsat@users.noreply.github.com>
2023-08-31 12:00:57 +0300

    Update Shop.h

--- Shop.h
+++ Shop.h
@@ -1,57 +1,58 @@
 #pragma once
 #include <iostream>
-using namespace std;
+
 
 class Shop
 {
 public:
-	virtual void showCharacteristics() = 0
-	{
-
-	}
+	virtual void showCharacteristics() = 0{	}
 	virtual void buy(int count) = 0;
 	virtual ~Shop() = default;
 };
 
 
-
 class Device : virtual public Shop
 {
 public:
-	Device(int count, int price, string IMEI, string company) : _count(count), _price(price), _IMEI(IMEI), _company(company)
-	{
-
-	}
+	Device(int count, int price, std::string & IMEI, std::string & company) : _count(count), _price(price), _IMEI(IMEI), _company(company){}
 
 	virtual void showCharacteristics() override
 	{
-		cout << "Device: count, price, IMEI." << endl;
-		cout << "Count: " << _count << endl;
-		cout << "Price: " << _price << endl;
-		cout << "IMEI: " << _IMEI << endl;
-		cout << "Company: " << _company << endl;
+		std::cout << "Device: count, price, IMEI." << std::endl;
+		std::cout << "Count: " << _count << std::endl;
+		std::cout << "Price: " << _price << std::endl;
+		std::cout << "IMEI: " << _IMEI << std::endl;
+		std::cout << "Company: " << _company << std::endl;
 	}
 	virtual void buy(int count) override
 	{
+		if(count < _count)
+			_count -= count;
 	}
+protected:
 	int _count;
 	int _price;
-	string _company;
-	string _IMEI;
+	std::string _company;
+	std::string _IMEI;
 };
 
-class MoreChar : virtual public Shop
+class MoreCharacteristics : virtual public Shop
 {
 public:
-	//MoreChar() = default;
-	MoreChar(int memory, string processor, bool updatable) : _memory(memory), _processor(processor), _updatable(updatable)
-	{
+	//MoreCharacteristics() = default;
+	MoreCharacteristics(int memory, std::string & processor, bool updatable) : _memory(memory), _processor(processor), _updatable(updatable){ }
 
+	MoreCharacteristics()
+	{
+		_memory = -1;
+		_processor = "DEFAULT";
+		_updatable = false;
 	}
 
 protected:
 	int _memory;
-	string _processor;
+	std::string _processor;
 	bool _updatable;
 };
+
 
