cbca8b1261da7f35b2f6872c464389c516e91d08
Smokedsat <65419292+smokedsat@users.noreply.github.com>
2023-08-31 11:59:02 +0300

    Update PC.h

--- PC.h
+++ PC.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "Shop.h"
 
-class PC final : virtual public Device,virtual public MoreChar
+class PC final : virtual public Device,virtual public MoreCharacteristics
 {
 public:
-	PC(int count, int price, string IMEI, string company, bool mouse, bool keyboard,int memory, string processor, bool updatable) : Device(count,price,IMEI, company), MoreChar(memory,processor,updatable), _mouse(mouse), _keyboard(keyboard) {};
+	PC(int count, int price, std::string &IMEI, std::string &company, bool mouse, bool keyboard,int memory, std::string &processor, bool updatable) : Device(count,price,IMEI, company), MoreCharacteristics(memory,processor,updatable), _mouse(mouse), _keyboard(keyboard) {};
 	virtual void showCharacteristics() override;
 	virtual void buy(int count) override;
 protected:
